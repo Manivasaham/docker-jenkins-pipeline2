b413e8631d9ae6c45fe3cdf507ed971b1ba8d945
Manivasaham <mylmanivasaham@gmail.com>
2022-12-25 13:14:31 +0530

    updated print stmt

--- src/main/java/com/example/demo/DockerJenkinsPipeline2.java
+++ src/main/java/com/example/demo/DockerJenkinsPipeline2.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class DockerJenkinsPipeline2 {
 
+
+	public String message() { return "welcome to docker-jenkins intergation"; }
 	public static void main(String[] args) {
 		SpringApplication.run(DockerJenkinsPipeline2.class, args);
 	}
